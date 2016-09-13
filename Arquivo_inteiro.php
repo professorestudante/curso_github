1ed28f72f4b8e7d522d77a9f5dfb4b836fb7eab9
Francisco Filho <prof.francisco2014@gmail.com>
2016-09-13 14:39:41 -0300

    Envio arquivo inteiro

---
+++ Arquivo_inteiro.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>School of Net</title>
+</head>
+<body>
+	<div id="estrutura">
+		<div id="topo">
+			<h1>School of Net - Site a Jato</h1>
+		</div>
+	<div id="menu">
+	<ul>
+		<li><a href="index.php">Home</a></li>
+		<li><a href="index.php">Empresa</a></li>
+		<li><a href="index.php">Produtos</a></li>
+		<li><a href="index.php">Contato</a></li>
+		
+	</ul>	
+	</div>
+	<hr />
+	
+	<div id="conteudo">
+		<h3>Lista de conteúdos</h3>
+	</div>
+	<hr />
+	
+	<div id="footer">
+		<h5>School of Net - Todos os direitos reservados</h5>
+	</div>
+	</div>
+</body>
+</html>
